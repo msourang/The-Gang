f9907aa50002c4293a1b2e29e2f426ee0ca3ded5
sweat9 <56995613+sweat9@users.noreply.github.com>
2021-08-14 22:19:17 +0200

    Popup + Tweaks

---
+++ index.js
@@ -0,0 +1,7 @@
+function showPopup() {
+    document.getElementById("popup").classList.remove('inactive');
+}
+
+function closePopup() {
+    document.getElementById('popup').classList.add('inactive');
+}
